6b0437f0a5fabca8f3758e5aa25671cd3b561bc1
Oscar Romero <ozromero7@gmail.com>
2022-08-04 01:29:48 -0500

    creacion de metodos para personaje

---
+++ Personaje/DeletePersonaje.php
@@ -0,0 +1,28 @@
+<?php
+
+    require "conexion.php";
+
+    if ($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+    
+        $ID_PERSONAJE = $_GET['ID_PERSONAJE'];
+        
+        $sql = "SELECT ID_PERSONAJE FROM personaje WHERE ID_PERSONAJE='$ID_PERSONAJE'";
+        $query = $mysqli->query($sql);
+        $resultado = $query->num_rows;
+        // echo($resultado);
+        
+        if($resultado==0){
+            
+            echo "No existe el personaje";
+        }else{
+            
+            $sql = "DELETE FROM personaje WHERE ID_PERSONAJE='$ID_PERSONAJE'";
+            $query = $mysqli->query($sql);
+            echo "Personaje eliminado correctamente";
+        }
+
+    }else{
+        header("HTTP/1.1 501 Not Implemented");
+        echo "Error de metodo";
+    }
+?>
---
+++ Personaje/PostPersonaje.php
@@ -0,0 +1,27 @@
+<?php
+
+    require "conexion.php";
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $ID_PERSONAJE = $_POST['ID_PERSONAJE'];
+        $ID_ESCUELA = $_POST['ID_ESCUELA'];
+        $ID_CASA = $_POST['ID_CASA'];
+        $NOMBRE_PERSONAJE = $_POST['NOMBRE_PERSONAJE'];
+        $APELLIDO_PERSONAJE = $_POST['APELLIDO_PERSONAJE'];
+        $FECHA_NACIMIENTO_PERSONAJE = $_POST['FECHA_NACIMIENTO$FECHA_NACIMIENTO_PERSONAJE'];
+        $RAZA_PERSONAJE = $_POST['RAZA_PERSONAJE'];
+        $PAPEL_PERSONAJE = $_POST['PAPEL_PERSONAJE'];
+        $FOTO_PERSONAJE = $_POST['FOTO_PERSONAJE'];
+        $ACTOR_PERSONAJE = $_POST['ACTOR_PERSONAJE'];
+       
+        $sql = "INSERT INTO personaje VALUES('$ID_UNIVERSO', '$ID_LIBRO', '$ID_PELICULA', '$TITULO_PELICULA', '$DIRECTOR_PELICULA', '$PRODUCTORA_PELICULA', '$ANO_ESTRENO_PELICULA', '$SINOPSIS_PELICULA')";
+        
+        $query = $mysqli->query($sql);
+        echo "Pelicula guardada correctamente";   
+    }else{
+        header("HTTP/1.1 501 Not Implemented");
+        echo "Error de metodo";
+    }
+    
+?>
