19ea5d47cf95da50669fcd903ac68318ba227cfd
Oscar Romero <ozromero7@gmail.com>
2022-08-04 01:18:47 -0500

    actualizaci[on de PUT de pelicula

--- Pelicula/PutPelicula.php
+++ Pelicula/PutPelicula.php
@@ -4,19 +4,17 @@
 
     if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
 
-        $ID_UNIVERSO = $_PUT ['ID_UNIVERSO'];
-        $ID_LIBRO = $_PUT ['ID_LIBRO'];
-        $ID_PELICULA = $_PUT ['ID_PELICULA'];
-        $TITULO_PELICULA = $_PUT ['TITULO_PELICULA'];
-        $DIRECTOR_PELICULA = $_PUT ['DIRECTOR_PELICULA'];
-        $PRODUCTORA_PELICULA = $_PUT ['PRODUCTORA_PELICULA'];
-        $ANO_ESTRENO_PELICULA = $_PUT ['ANO_ESTRENO_PELICULA'];
-        $SINOPSIS_PELICULA = $_PUT ['SINOPSIS_PELICULA'];
+        $ID_PELICULA = $_GET ['ID_PELICULA'];
+        $TITULO_PELICULA = $_GET ['TITULO_PELICULA'];
+        $DIRECTOR_PELICULA = $_GET ['DIRECTOR_PELICULA'];
+        $PRODUCTORA_PELICULA = $_GET ['PRODUCTORA_PELICULA'];
+        $ANO_ESTRENO_PELICULA = $_GET ['ANO_ESTRENO_PELICULA'];
+        $SINOPSIS_PELICULA = $_GET ['SINOPSIS_PELICULA'];
         
         $sql = "UPDATE pelicula SET TITULO_PELICULA='$TITULO_PELICULA', DIRECTOR_PELICULA='$DIRECTOR_PELICULA', PRODUCTORA_PELICULA='$PRODUCTORA_PELICULA', ANO_ESTRENO_PELICULA='$ANO_ESTRENO_PELICULA', SINOPSIS_PELICULA='$SINOPSIS_PELICULA' WHERE ID_PELICULA='$ID_PELICULA'";
         
         $query = $mysqli->query($sql);
-        echo "Libro actualizado correctamente";
+        echo "Pelicula actualizada correctamente";
     }else{
         header("HTTP/1.1 501 Not Implemented");
         echo "Error de metodo";
