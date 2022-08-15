08377d166de5f1b1f1605967e052e1cbd3e26a97
Oscar Romero <ozromero7@gmail.com>
2022-08-15 14:39:42 -0500

    modificaciones de datos

--- Casas/PutCasas.php
+++ Casas/PutCasas.php
@@ -4,13 +4,14 @@
 
     if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
 
+        $ID_ESCUELA = $_GET['ID_ESCUELA'];
         $ID_CASA = $_GET['ID_CASA'];
         $NOMBRE_CASA = $_GET['NOMBRE_CASA'];
         $COLOR_CASA = $_GET['COLOR_CASA'];
         $VIRTUD_CASA = $_GET['VIRTUD_CASA'];
         $NOMBRE_FANTASMA_CASA = $_GET['NOMBRE_FANTASMA_CASA'];
         
-        $sql = "UPDATE casa SET NOMBRE_CASA='$NOMBRE_CASA', COLOR_CASA='$COLOR_CASA', VIRTUD_CASA='$VIRTUD_CASA', NOMBRE_FANTASMA_CASA='$NOMBRE_FANTASMA_CASA' WHERE ID_CASA='$ID_CASA'";
+        $sql = "UPDATE casa SET ID_ESCUELA ='$ID_ESCUELA', NOMBRE_CASA='$NOMBRE_CASA', COLOR_CASA='$COLOR_CASA', VIRTUD_CASA='$VIRTUD_CASA', NOMBRE_FANTASMA_CASA='$NOMBRE_FANTASMA_CASA' WHERE ID_CASA='$ID_CASA'";
         
         $query = $mysqli->query($sql);
         echo "Casa actualizada correctamente";
